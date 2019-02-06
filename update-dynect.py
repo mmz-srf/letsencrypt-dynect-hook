2009090822289d6fa9beb6758c01f7b001988a1b
Claudio Walser <claudio.walser@srf.ch>
2019-02-06 07:22:09 +0100

    pep8 code statndards

--- update-dynect.py
+++ update-dynect.py
@@ -9,66 +9,78 @@ from dynect.DynectDNS import DynectRest
 
 class LetsencryptDynectUpdater (object):
 
-	customer_name = os.environ['CERTBOT_CUSTOMER_NAME']
-	api_key = os.environ['CERTBOT_API_KEY']
-	api_pass = os.environ['CERTBOT_API_PASS']
-	challengeSubdomain = '_acme_challenge'
-	rest_client = DynectRest()
-	fqdn = None
-	domain = None
-	validation = None
+    customer_name = os.environ['CERTBOT_CUSTOMER_NAME']
+    api_key = os.environ['CERTBOT_API_KEY']
+    api_pass = os.environ['CERTBOT_API_PASS']
+    challengeSubdomain = '_acme_challenge'
+    rest_client = DynectRest()
+    fqdn = None
+    domain = None
+    validation = None
 
-	def __init__(self, domain, validation):
-		self.domain = domain
-		self.validation = validation
-		self.fqdn = '{}.{}'.format(self.challengeSubdomain, self.domain)
+    def __init__(self, domain, validation):
+        self.domain = domain
+        self.validation = validation
+        self.fqdn = '{}.{}'.format(self.challengeSubdomain, self.domain)
 
-	def login(self):
-		arguments = {
-			'customer_name': self.customer_name,
-			'user_name': self.api_key,  
-			'password': self.api_pass,
-		}
+    def login(self):
+        arguments = {
+            'customer_name': self.customer_name,
+            'user_name': self.api_key,
+            'password': self.api_pass,
+        }
 
-		response = self.rest_client.execute('/Session/', 'POST', arguments)
-		if response['status'] != 'success':
-			print("Incorrect credentials")
-			sys.exit(1)
+        response = self.rest_client.execute('/Session/', 'POST', arguments)
+        if response['status'] != 'success':
+            print("Incorrect credentials")
+            sys.exit(1)
 
-	def deleteValidationRecord(self):
-		response = self.rest_client.execute('/TXTRecord/{}/{}/'.format(self.domain, self.fqdn), 'DELETE')
-		if response['status'] != 'success':
-			print('Failed to delete txt entries')
-			sys.exit(1)
+    def deleteValidationRecord(self):
+        response = self.rest_client.execute(
+            '/TXTRecord/{}/{}/'.format(
+                self.domain,
+                self.fqdn
+            ), 'DELETE'
+        )
+        if response['status'] != 'success':
+            print('Failed to delete txt entries')
+            sys.exit(1)
 
-		records = response['data']
-		for record in records:
-			record_id = record.split('/').pop()
-			print(record_id)
-			print(record)
-		return True
+        records = response['data']
+        for record in records:
+            record_id = record.split('/').pop()
+            print(record_id)
+            print(record)
+        return True
 
-	def addValidationRecord(self):
-		arguments = {
-			'rdata': {'txtdata': self.validation},
-			'ttl': 5
-		}
-		response = self.rest_client.execute('/TXTRecord/{}/{}/'.format(self.domain, self.fqdn), 'POST', arguments)
-		if response['status'] != 'success':
-			print('Adding txt record failed')
-			sys.exit(1)
+    def addValidationRecord(self):
+        arguments = {
+            'rdata': {'txtdata': self.validation},
+            'ttl': 5
+        }
+        response = self.rest_client.execute(
+            '/TXTRecord/{}/{}/'.format(
+                self.domain,
+                self.fqdn
+            ), 'POST', arguments)
+        if response['status'] != 'success':
+            print('Adding txt record failed')
+            sys.exit(1)
 
-	def publish(self):	
-		arguments = {
-			'publish': 1
-		}
-		response = self.rest_client.execute('/Zone/{}/'.format(self.domain), 'PUT', arguments)
-		if response['status'] != 'success':
-			print('Adding txt record failed')
-			sys.exit(1)
+    def publish(self):
+        arguments = {
+            'publish': 1
+        }
+        response = self.rest_client.execute(
+            '/Zone/{}/'.format(
+                self.domain
+            ), 'PUT', arguments)
+        if response['status'] != 'success':
+            print('Adding txt record failed')
+            sys.exit(1)
 
-	def logout(self):
-		self.rest_client.execute('/Session/', 'DELETE')
+    def logout(self):
+        self.rest_client.execute('/Session/', 'DELETE')
 
 
 # arguments
@@ -92,4 +104,3 @@ updater.deleteValidationRecord()
 updater.addValidationRecord()
 updater.publish()
 updater.logout()
-
