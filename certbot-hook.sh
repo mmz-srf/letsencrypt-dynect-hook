32d5ca88a28024637b8f45f2a38d34ae343ea7e8
Claudio Walser <claudio.walser@srf.ch>
2019-03-12 10:01:35 +0100

    Update certbot-hook.sh

--- certbot-hook.sh
+++ certbot-hook.sh
@@ -11,6 +11,4 @@ source /etc/environment
 
 /usr/bin/python3 $(pwd)/update-dynect.py $CERTBOT_DOMAIN $CERTBOT_VALIDATION
 EXIT_CODE=$?
-#echo "Now waiting for 60 seconds"
-#sleep 60
 exit $EXIT_CODE
