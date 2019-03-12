238a66c5906e8d0143b378a06ec2e01577094578
Claudio Walser <claudio.walser@srf.ch>
2019-03-12 09:35:00 +0100

    Update certbot-hook.sh

--- certbot-hook.sh
+++ certbot-hook.sh
@@ -9,6 +9,7 @@ fi
 # load env variables since they are not available in a dynect hook by default
 source /etc/environment
 
+echo "Adding ${CERTBOT_VALIDATION} for ${CERTBOT_DOMAIN}"
 /usr/bin/python3 $(pwd)/update-dynect.py $CERTBOT_DOMAIN $CERTBOT_VALIDATION
 echo "Now waiting for 60 seconds"
 sleep 60
