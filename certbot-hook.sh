9a8301454839aedfc888c912bfb1a1f9be9019ec
Claudio Walser <claudio.walser@srf.ch>
2019-12-18 14:06:07 +0100

    Update certbot-hook.sh

--- certbot-hook.sh
+++ certbot-hook.sh
@@ -10,12 +10,6 @@ fi
 
 # load env variables since they are not available in a dynect hook by default
 source /etc/environment
-echo "From cat"
-cat /etc/environment
-echo "Printenv"
-printenv
-exit 1
-
 
 /usr/bin/python3 $(pwd)/update-dynect.py $CERTBOT_DOMAIN $CERTBOT_VALIDATION
 EXIT_CODE=$?
