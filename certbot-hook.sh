efaeef5299bb9fd9e5f8c1446eb5f93dfd286735
Claudio Walser <claudio.walser@srf.ch>
2020-03-26 15:12:15 +0100

    Add entry on nsone

--- certbot-hook.sh
+++ certbot-hook.sh
@@ -7,7 +7,7 @@ function authDynect() {
 
 
 function authNsone() {
-    echo lexicon "nsone" "--auth-token=${NSONE_API_KEY}" \
+    lexicon "nsone" "--auth-token=${NSONE_API_KEY}" \
     create "${CERTBOT_DOMAIN_CLEAN}" TXT --name "_acme-challenge.${CERTBOT_DOMAIN_CLEAN}" --content "${CERTBOT_VALIDATION}"
     return $?
 }
